f19b4c07ff2437ab40d46fe15793df360c6c7dcd
Priyanshu078 <priyanshupaliwal078@gmail.com>
2023-07-19 23:23:47 +0530

    learning firebase messaging

--- lib/firebase_code.dart
+++ lib/firebase_code.dart
@@ -1,4 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  print("Handling a background message: ${message.notification!.title}");
+}
 
 class FirebaseApi {
   var firebaseMessaing = FirebaseMessaging.instance;
@@ -17,5 +25,7 @@ class FirebaseApi {
             'Message also contained a notification: ${message.notification!.title}');
       }
     });
+
+    FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   }
 }
